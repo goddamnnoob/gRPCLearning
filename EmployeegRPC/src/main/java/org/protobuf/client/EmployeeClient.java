256113c4bd1f375742287119c65906ce995fc3cf
gowtham-14117 <gowtham-14117@csez.zohocorpin.com>
2023-11-10 13:15:06 +0530

    Implemented 2 Synchronous requests

--- EmployeegRPC/src/main/java/org/protobuf/client/EmployeeClient.java
+++ EmployeegRPC/src/main/java/org/protobuf/client/EmployeeClient.java
@@ -6,6 +6,8 @@ import org.protobuf.EmployeeRequest;
 import org.protobuf.EmployeeResponse;
 import org.protobuf.EmployeeServiceGrpc;
 
+import java.util.ArrayList;
+
 public class EmployeeClient {
     public static void main (String[] args){
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",4666)
@@ -13,11 +15,18 @@ public class EmployeeClient {
                 .build();
 
         EmployeeServiceGrpc.EmployeeServiceBlockingStub stub = EmployeeServiceGrpc.newBlockingStub(channel);
-        EmployeeRequest employeeRequest = EmployeeRequest.newBuilder()
-                .setEmployeeId(123)
-                .build();
-        EmployeeResponse employeeResponse = stub.getEmployee(employeeRequest);
-        System.out.println("############# Firstname: "+employeeResponse.getFirstName());
-        System.out.println("############# LastName: "+employeeResponse.getLastName());
+        ArrayList<EmployeeRequest>  employeeRequests = new ArrayList<>();
+        for(int i=0;i<2;i++){
+            EmployeeRequest employeeRequest = EmployeeRequest.newBuilder()
+                    .setEmployeeId(123+i)
+                    .build();
+            employeeRequests.add(employeeRequest);
+        }
+        for(int i=0;i<2;i++){
+            EmployeeResponse employeeResponse = stub.getEmployee(employeeRequests.get(i));
+            System.out.println("############# Firstname: "+employeeResponse.getFirstName());
+            System.out.println("############# LastName: "+employeeResponse.getLastName());
+        }
+
     }
 }
